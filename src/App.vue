6c31bf42c43cf8abcf5cf10d729d85c7b08d25bb
itachon <ph.longeri@gmail.com>
2023-09-18 23:35:49 -0300

    Cambios menores

--- src/App.vue
+++ src/App.vue
@@ -19,8 +19,8 @@
                     </li>
                     <li>
                         <button
-                            class="mb-2 md:mb-0 bg-red-500 border border-red-500 px-5 py-2 text-sm shadow-sm font-medium tracking-wider text-white rounded-full hover:shadow-lg hover:bg-red-600"
-                            @click="restart">REINICIAR</button>
+                            class="mb-2 md:mb-0 bg-red-500 border border-red-500 px-5 py-2 text-xs shadow-sm font-medium tracking-wider text-white rounded-full hover:shadow-lg hover:bg-red-600"
+                            @click="restart">RESTART GAME</button>
                     </li>
                 </ul>
             </div>
